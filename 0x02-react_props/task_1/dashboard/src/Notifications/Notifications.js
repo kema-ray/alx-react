795ba0f7391e78e0e9f8cde638e2bc91fb5054b6
kema-ray <rachelkemuma99@gmail.com>
2023-08-31 15:04:57 +0300

    Write the tests for each component

---
+++ 0x02-react_props/task_1/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Notifications.css';
+import { getLatestNotification } from '../utils'
+import closeIcon from './close-icon.png'
+
+
+function Notifications() {
+    return (
+        <div className='Notifications'>
+            <p style={{ display: 'inline', marginRight: '80%'}}>
+                Here is the list of notifications
+            </p>
+            <button aria-label='Close'
+            onClick={console.log('Close button has been clicked')}>
+                <img style={{ display: 'inline'}} src={closeIcon} alt='Close' />
+            </button>
+            <ul>
+                <li data='default'>New course available</li>
+                <li data='urgent'>New resume available</li>
+                <li
+					data='urgent'
+					dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+				></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Notifications;
