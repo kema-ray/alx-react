dbf0ffb4507e9fdf10e647ed9deeef4992283c79
kema-ray <rachelkemuma99@gmail.com>
2023-09-04 10:58:29 +0300

    make changes

--- 0x02-react_props/task_4/dashboard/src/App/App.js
+++ 0x02-react_props/task_4/dashboard/src/App/App.js
@@ -6,28 +6,41 @@ import Login from '../Login/Login';
 import CourseList from '../CourseList/CourseList';
 import PropTypes from 'prop-types';
 import Notifications from '../Notifications/Notifications';
+import { getLatestNotification } from "../utils/utils";
 
-function App() {
-  return (
-    <React.Fragment>
-      <div className='App'>
+const listCourses = [
+	{ id: 1, name: 'ES6', credit: 60 },
+	{ id: 2, name: 'Webpack', credit: 20 },
+	{ id: 3, name: 'React', credit: 40 },
+];
+
+const listNotifications = [
+	{ id: 1, type: 'default', value: 'New course available' },
+	{ id: 2, type: 'urgent', value: 'New resume available' },
+	{ id: 3, type: 'urgent', html: getLatestNotification() },
+];
+
+function App({ isLoggedIn }) {
+	return (
+		<React.Fragment>
+			<div className='App'>
 				<div className='heading-section'>
-					<Notifications />
+					<Notifications listNotifications={listNotifications} />
 					<Header />
 				</div>
-				{isLoggedIn ? <CourseList /> : <Login />}
+				{isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
 				<Footer />
 			</div>
-    </React.Fragment>
-  );
+		</React.Fragment>
+	);
 }
 
 App.defaultProps = {
-  isLoggedIn: false,
+	isLoggedIn: false,
 };
 
 App.propTypes = {
-  isLoggedIn: PropTypes.bool,
+	isLoggedIn: PropTypes.bool,
 };
 
 export default App;
