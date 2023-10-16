ec4d2c3284125f0922686fb02b8438aeedb5d788
kema-ray <rachelkemuma99@gmail.com>
2023-10-16 17:21:01 +0300

    Connect notifications: Improve reducer

--- 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
@@ -1,45 +1,24 @@
-import {
-	FETCH_NOTIFICATIONS_SUCCESS,
-	MARK_AS_READ,
-	SET_TYPE_FILTER,
-} from '../actions/notificationActionTypes';
-import { notificationsNormalizer } from '../schema/notifications';
+import { FETCH_NOTIFICATIONS_SUCCESS, MARK_AS_READ, SET_LOADING_STATE, SET_TYPE_FILTER } from "../actions/notificationActionTypes";
+import { Map, setIn } from "immutable";
 
-const initialState = Map({
-	notifications: [],
-	filter: 'DEFAULT',
-});
 
-export default function notificationReducer(state = initialState, action) {
-	switch (action.type) {
-		case FETCH_NOTIFICATIONS_SUCCESS: {
-			const notificationData = action.data.map((item) => ({
-				id: item.id,
-				type: item.type,
-				value: item.value,
-				isRead: false,
-			}));
-			return state.merge({
-				filter: NotificationTypeFilters.DEFAULT,
-				notifications: notificationsNormalizer(notificationData),
-			});
-		}
-		case MARK_AS_READ: {
-			return Map(state).setIn(
-				[
-					'notifications',
-					'entities',
-					'notifications',
-					action.index.toString(),
-					'isRead',
-				],
-				true
-			);
-		}
-		case SET_TYPE_FILTER: {
-			return Map(state).set('filter', action.filter);
-		}
-		default:
-			return state;
-	}
+export const initialState = Map({
+  notifications: {},
+  filter: "DEFAULT",
+  loading: false
+})
+
+export function notificationReducer(state = initialState, action){
+  switch(action.type){
+    case FETCH_NOTIFICATIONS_SUCCESS:
+      return state.mergeDeep({notifications: action.data})
+    case MARK_AS_READ:
+      return setIn(state.toJS(), ["notifications", action.index, "isRead"], true)
+    case SET_TYPE_FILTER:
+      return state.set("filter", action.filter)
+    case SET_LOADING_STATE:
+      return state.set("loading", action.loading)
+    default:
+      return state
+  }
 }
