f255554c4dc230c45c9e42f7531d1bce1c69df04
kema-ray <rachelkemuma99@gmail.com>
2023-10-16 17:11:17 +0300

    Combine store: modify the application

--- 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App/App';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import { uiReducer } from './reducers/uiReducers';
+// import { uiReducer } from './reducers/uiReducers';
 import thunk from 'redux-thunk';
+import { rootReducer } from './reducers/rootReducer';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
-  uiReducer,
+  rootReducer,
   composeEnhancers(applyMiddleware(thunk))
 )
 
